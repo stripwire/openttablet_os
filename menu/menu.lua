e8e1e0a0dce903d785bf0192953f894528a38265
stripwire <30651037+stripwire@users.noreply.github.com>
2017-08-18 13:30:41 +0300

    Add files via upload

---
+++ menu/menu.lua
@@ -0,0 +1,19 @@
+local lib-forms=require('forms')
+
+
+
+---формы
+menu=forms.addform()
+menu.border=1
+menu.fontcolor=0x1E90FF
+--
+---
+
+---кнопки
+nanob=menu:addButton(left,top,nano,onClick)
+nanob.w=10
+nanob.h=10
+nanob.border=1
+nanob.fontcolor=0xFFD700
+--
+---
